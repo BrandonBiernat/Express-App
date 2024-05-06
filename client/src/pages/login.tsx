2f5171b0fd3bf5938ae2c3f5dc9103b73bf785e2
Brandon Biernat <brandon.biernat@students.pcci.edu>
2024-05-06 14:19:51 -0500

    Configured routing

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+    const container = {
+
+    };
+
+    return (
+        <div style={{
+            justifyContent: "center",
+            display: "flex"
+        }}>
+            <div>
+                Login
+            </div>
+        </div>
+    );
+}
+
+export default Login;

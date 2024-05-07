dd15c9a637a3557bb00cdafd86422e13afbee6d4
Brandon Biernat <brandon.biernat@students.pcci.edu>
2024-05-06 21:32:32 -0500

    started work on login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 const Login = () => {
     const container = {
+        justifyContent: "center",
+        display: "flex"
+    };
 
+    const loginArea = {
+        border: 'none',
+        borderRadius: '5px',
+        boxShadow: '3px 3px',
+        padding: '10px'
     };
 
     return (
-        <div style={{
-            justifyContent: "center",
-            display: "flex"
-        }}>
-            <div>
+        <div style={container}>
+            <div style={loginArea}>
                 Login
             </div>
         </div>
